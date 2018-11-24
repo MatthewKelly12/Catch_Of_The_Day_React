fae3dac8236e7e2b05e856b5410dbc388de58d6e
Matthew Kelly <mkonabike@gmail.com>
2018-11-24 08:44:21 -0600

    added Router

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter, Route, Switch, } from 'react-router-dom'
+import StorePicker from './StorePicker'
+
+
+const NotFound = () => (
+	<div>
+		<h2>Error! Not Found!!!!</h2>
+	</div>
+)
+
+export default NotFound
