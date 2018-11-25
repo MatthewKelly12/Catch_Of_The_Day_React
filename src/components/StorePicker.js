3fee1dd5060af1eccb54e12d6cf5574c8f0ab961
Matthew Kelly <mkonabike@gmail.com>
2018-11-24 18:36:49 -0600

    added click event to store picker input, changing pages, entering store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+	myInput = React.createRef()
+
+	goToStore = (event) => {
+		// STOP FORM FROM SUBMITTING
+		event.preventDefault()
+		// GET TEXT FROM INPUT
+		const storeName = this.myInput.value.value
+		// CHANGE THE PAGE TO /store/{textinput}
+		this.props.history.push(`/store/${storeName}`)
+	}
+
+
 	render () {
 	return (
-		<form className='store-selector'>
+		<form className='store-selector' onSubmit={this.goToStore}>
 			<h2>Please Enter A Store</h2>
-			<input type='text' placeholder='Store Name'></input>
+			<input
+				type='text'
+				ref={this.myInput}
+				required
+				placeholder='Store Name'
+				defaultValue={getFunName()}
+			/>
 			<button type='submit'>Visit Store -></button>
 		</form>
 	)
