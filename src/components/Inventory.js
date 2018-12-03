b9c10e60bcd4f0f3e3bb17c2e74b2186348759b4
Matthew Kelly <mkonabike@gmail.com>
2018-12-02 21:00:25 -0600

    added authentication with firebae

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import firebase from 'firebase'
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
-import PropTypes from 'prop-types'
+import Login from './Login';
+import base, { firebaseApp } from '../base'
 
 class Inventory extends React.Component {
 
@@ -11,10 +14,71 @@ class Inventory extends React.Component {
 		deleteFish:	PropTypes.func,
 		loadSampleFishes: PropTypes.func
 	}
+
+	state = {
+		uid: null,
+		owner: null
+	}
+
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.authHandler({user})
+			}
+		})
+	}
+
+	authHandler = async authData => {
+		// LOOK UP CURRENT STORE IN FIREBASE DATABASE
+		const store = await base.fetch(this.props.storeId, { context:this })
+		console.log(store)
+		// CLAIM STORE IF NO OWNER
+		if (!store.owner) {
+			// SAVE STORE AS OUR OWN
+			await base.post(`${this.props.storeId}/owner`, {
+				data: authData.user.uid
+			})
+		}
+		// SET STATE OF CURRENT INVENTORY TO REFLECT CURRENT USER
+		this.setState({
+			uid: authData.user.uid,
+			owner: store.owner || authData.user.uid
+		})
+		console.log(authData)
+	}
+	authenticate = (provider) => {
+		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+		firebaseApp
+			.auth()
+			.signInWithPopup(authProvider)
+			.then(this.authHandler)
+	}
+
+	logout = async () => {
+		console.log('logging out')
+		await firebase.auth().signOut()
+		this.setState({uid:null})
+	}
 	render () {
+		const logout = <button onClick={this.logout}>Log Out</button>
+		// CHECK TO SEE IF USER IS LOGGED IN
+		if (!this.state.uid) {
+			return <Login authenticate={this.authenticate}/>
+		}
+		// CHECK TO SEE IF USER IS NOT OWNER OF THE STORE
+		if (this.state.uid !== this.state.owner) {
+			return (
+				<div>
+					<p>Sorry you are not the owner!</p>
+					{logout}
+				</div>
+			)
+		}
+		// OWNER OF STORE, RENDER INVENTORY
 		return (
 			<div>
 				<h2>Inventory!</h2>
+				{logout}
 				{
 				Object.keys(this.props.fishes)
 					.map(key =>
